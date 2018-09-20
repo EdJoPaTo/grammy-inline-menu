0cd117ee67c1abb79c19370dda29ab9f7b0c2528
Edgar To <edjopato@gmail.com>
2018-09-20 22:42:10 +0200

    docs(example): use a food select menu as new example

--- example.js
+++ example.js
@@ -37,26 +37,77 @@ menu.select('s', ['A', 'B', 'C'], {
   isSetFunc: (ctx, key) => key === selectedKey
 })
 
-menu.question('Frage', 'f', {
-  questionText: 'Was willst du schon immer loswerden?',
-  setFunc: (ctx, answer) => ctx.reply(answer),
+const someMenu = new TelegrafInlineMenu('People like food. What do they like?')
+
+const people = {Mark: {}, Paul: {}}
+const food = ['bread', 'cake', 'bananas']
+
+function personButtonOptions() {
+  const result = {}
+  Object.keys(people)
+    .forEach(person => {
+      const choise = people[person].food
+      if (choise) {
+        result[person] = `${person} (${choise})`
+      } else {
+        result[person] = person
+      }
+    })
+  return result
+}
+
+function foodSelectText(ctx) {
+  const person = ctx.match[1]
+  const hisChoice = people[person].food
+  if (!hisChoice) {
+    return `${person} is still unsure what to eat.`
+  }
+  return `${person} likes ${hisChoice} currently.`
+}
+
+const foodSelectSubmenu = new TelegrafInlineMenu(foodSelectText)
+  .toggle('Prefer Tee', 't', {
+    setFunc: (ctx, choice) => {
+      const person = ctx.match[1]
+      people[person].tee = choice
+    },
+    isSetFunc: ctx => {
+      const person = ctx.match[1]
+      return people[person].tee === true
+    }
+  })
+  .select('f', food, {
+    setFunc: (ctx, key) => {
+      const person = ctx.match[1]
+      people[person].food = key
+    },
+    isSetFunc: (ctx, key) => {
+      const person = ctx.match[1]
+      return people[person].food === key
+    }
+  })
+
+someMenu.select('p', personButtonOptions, {
+  submenu: foodSelectSubmenu,
+  columns: 2
+})
+
+someMenu.question('Add person', 'add', {
+  questionText: 'Who likes food too?',
+  setFunc: (ctx, key) => {
+    people[key] = {}
+  }
+})
+
+menu.submenu('Food menu', 'b', someMenu, {
   hide: () => mainMenuToggle
 })
 
-const someMenu = new TelegrafInlineMenu('Other Menu')
-someMenu.button('other hit me', 'c', {
-  doFunc: ctx => ctx.answerCbQuery('other hit me')
-})
-
-someMenu.submenu('Third Menu', 'y', new TelegrafInlineMenu('Third Menu'))
+menu.submenu('Third Menu', 'y', new TelegrafInlineMenu('Third Menu'))
   .setCommand('third')
   .simpleButton('Just a button', 'a', {
     doFunc: ctx => ctx.answerCbQuery('Just a callback query answer')
   })
-
-menu.submenu('Other menu', 'b', someMenu, {
-  hide: () => mainMenuToggle
-})
 
 menu.setCommand('start')
 
