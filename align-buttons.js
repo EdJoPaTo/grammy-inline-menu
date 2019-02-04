4487dda2b07542a06e412be55f215d87ed1b0071
Edgar Toll <edjopato@gmail.com>
2019-02-04 13:53:22 +0100

    refactor(align-buttons): allow others to read the defaults

--- align-buttons.js
+++ align-buttons.js
@@ -1,4 +1,7 @@
-function getRowsOfButtons(buttons, columns = 6, maxRows = 10) {
+const DEFAULT_BUTTON_COLUMNS = 6
+const DEFAULT_BUTTON_ROWS = 10
+
+function getRowsOfButtons(buttons, columns = DEFAULT_BUTTON_COLUMNS, maxRows = DEFAULT_BUTTON_ROWS) {
   const maxButtons = Math.min(maxRows * columns, buttons.length)
   const rows = []
   for (let i = 0; i < maxButtons; i += columns) {
@@ -10,5 +13,7 @@ function getRowsOfButtons(buttons, columns = 6, maxRows = 10) {
 }
 
 module.exports = {
+  DEFAULT_BUTTON_COLUMNS,
+  DEFAULT_BUTTON_ROWS,
   getRowsOfButtons
 }
