162de1361cff105d3655ebc92e078cc2e392f374
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-25 01:19:19 +0200

    test(toggle): add tests

---
+++ source/buttons/toggle.test.ts
@@ -0,0 +1,84 @@
+import test from 'ava'
+
+import {generateToggleButton} from './toggle'
+
+test('hidden does not render any button', async t => {
+	const func = generateToggleButton('text', 'pre', {
+		hide: () => true,
+		isSet: () => {
+			t.fail('do not call as its hidden')
+			throw new Error('do not call as its hidden')
+		},
+		set: () => {
+			t.fail('do not call as its hidden')
+		}
+	})
+
+	const button = await func(undefined, 'wow/')
+	t.deepEqual(button, undefined)
+})
+
+test('is true button', async t => {
+	const func = generateToggleButton('text', 'pre', {
+		isSet: () => true,
+		set: () => {
+			t.fail('do not call as the button is not hit')
+		}
+	})
+
+	const button = await func(undefined, 'wow/')
+	t.deepEqual(button, {
+		text: '✅ text',
+		relativePath: 'pre:false'
+	})
+})
+
+test('is false button', async t => {
+	const func = generateToggleButton('text', 'pre', {
+		isSet: () => false,
+		set: () => {
+			t.fail('do not call as the button is not hit')
+		}
+	})
+
+	const button = await func(undefined, 'wow/')
+	t.deepEqual(button, {
+		text: '🚫 text',
+		relativePath: 'pre:true'
+	})
+})
+
+test('own format', async t => {
+	const func = generateToggleButton('text', 'pre', {
+		isSet: () => true,
+		set: () => {
+			t.fail('do not call as the button is not hit')
+		},
+		formatState: (_context, text, state) => {
+			t.is(text, 'text')
+			t.is(state, true)
+			return 'lalala'
+		}
+	})
+
+	const button = await func(undefined, 'wow/')
+	t.deepEqual(button, {
+		text: 'lalala',
+		relativePath: 'pre:false'
+	})
+})
+
+test('async text', async t => {
+	const func = generateToggleButton(() => 'text', 'pre', {
+		isSet: () => true,
+		set: () => {
+			t.fail('do not call as the button is not hit')
+		}
+	})
+
+	const button = await func(undefined, 'wow/')
+	t.deepEqual(button, {
+		text: '✅ text',
+		relativePath: 'pre:false'
+	})
+})
