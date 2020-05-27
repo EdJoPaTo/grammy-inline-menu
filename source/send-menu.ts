9162303c1575a78796a0b7257c2c655c20b5ff80
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-27 17:20:43 +0200

    docs(send-menu): add jsdoc

--- source/send-menu.ts
+++ source/send-menu.ts
@@ -5,8 +5,18 @@ import {Body, TextBody, MediaBody, isMediaBody, getBodyText, jsUserBodyHints} fr
 import {InlineKeyboard} from './keyboard'
 import {MenuLike} from './menu-like'
 
+/**
+ * Generic Method which is able to send a menu to a context (given a path where it is)
+ */
 export type SendMenuFunc<Context> = (menu: MenuLike<Context>, context: Context, path: string) => Promise<unknown>
 
+/**
+ * Reply a menu to a context as a new message
+ * @param menu menu to be shown
+ * @param context current Telegraf context to reply the menu to it
+ * @param path path of the menu
+ * @param extra optional additional Telegraf Extra options
+ */
 export async function replyMenuToContext<Context extends TelegrafContext>(menu: MenuLike<Context>, context: Context, path: string, extra: Readonly<ExtraReplyMessage> = {}): Promise<Message> {
 	const body = await menu.renderBody(context, path)
 	jsUserBodyHints(body)
@@ -14,6 +24,13 @@ export async function replyMenuToContext<Context extends TelegrafContext>(menu: 
 	return replyRenderedMenuPartsToContext(body, keyboard, context, extra)
 }
 
+/**
+ * Edit the context into the menu. If thats not possible the current message is deleted and a new message is replied
+ * @param menu menu to be shown
+ * @param context current Telegraf context to edit the menu into
+ * @param path path of the menu
+ * @param extra optional additional Telegraf Extra options
+ */
 export async function editMenuOnContext<Context extends TelegrafContext>(menu: MenuLike<Context>, context: Context, path: string, extra: Readonly<ExtraEditMessage> = {}): Promise<void> {
 	const body = await menu.renderBody(context, path)
 	jsUserBodyHints(body)
@@ -60,6 +77,11 @@ export async function editMenuOnContext<Context extends TelegrafContext>(menu: M
 	])
 }
 
+/**
+ * Delete the message on the context.
+ * If thats not possible the reply markup (keyboard) is removed. The user can not press any buttons on that old message.
+ * @param context context of the message to be deleted
+ */
 export async function deleteMenuFromContext<Context extends TelegrafContext>(context: Context): Promise<void> {
 	try {
 		await context.deleteMessage()
@@ -68,6 +90,13 @@ export async function deleteMenuFromContext<Context extends TelegrafContext>(con
 	}
 }
 
+/**
+ * Deletes to menu of the current context and replies a new one ensuring the menu is at the end of the chat.
+ * @param menu menu to be shown
+ * @param context current Telegraf context to send the menu to
+ * @param path path of the menu
+ * @param extra optional additional Telegraf Extra options
+ */
 export async function resendMenuToContext<Context extends TelegrafContext>(menu: MenuLike<Context>, context: Context, path: string, extra: Readonly<ExtraReplyMessage> = {}): Promise<Message> {
 	const [menuMessage] = await Promise.all([
 		replyMenuToContext(menu, context, path, extra),
@@ -118,6 +147,11 @@ async function replyRenderedMenuPartsToContext<Context extends TelegrafContext>(
 	throw new Error('the body of the menu template can not be replied. It has to contain at least something replyable like text')
 }
 
+/**
+ * Generate a function to send the menu towards a chat from external events
+ * @param menu menu to be shown
+ * @param path path of the menu
+ */
 export function generateSendMenuToChatFunction<Context>(menu: MenuLike<Context>, path: string): (telegram: Readonly<Telegram>, chatId: string | number, context: Context, extra?: Readonly<ExtraReplyMessage>) => Promise<Message> {
 	return async (telegram, chatId, context, extra = {}) => {
 		const body = await menu.renderBody(context, path)
