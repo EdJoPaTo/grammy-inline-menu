f4113c04c233645d7f129e080844f4419b17c340
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-27 17:52:19 +0200

    test(action-hive): add tests

---
+++ source/action-hive.test.ts
@@ -0,0 +1,86 @@
+import test from 'ava'
+
+import {ActionHive, ActionFunc} from './action-hive'
+
+test('add nothing is empty', t => {
+	const a = new ActionHive()
+
+	const result = a.list(/^foo\//)
+	t.is(result.size, 0)
+})
+
+test('add simple doFunction', t => {
+	const a = new ActionHive()
+
+	const doFunction = (): never => {
+		throw new Error('dont call the action function on list')
+	}
+
+	a.add(/bar$/, doFunction, undefined)
+
+	const resultSet = a.list(/^foo\//)
+	t.is(resultSet.size, 1)
+	const result = [...resultSet][0]
+
+	t.truthy(new RegExp(result.trigger.source, result.trigger.flags).exec('foo/bar'))
+
+	t.deepEqual(result.trigger.source, '^foo\\/bar$')
+	t.deepEqual(result.trigger.flags, '')
+})
+
+test('doFunction without hide runs doFunction', async t => {
+	t.plan(3)
+	const a = new ActionHive<string>()
+
+	const doFunction: ActionFunc<string> = (context, path) => {
+		t.is(context, 'bob')
+		t.is(path, 'foo/bar')
+		return 'wow'
+	}
+
+	a.add(/bar$/, doFunction, undefined)
+
+	const resultSet = a.list(/^foo\//)
+	const result = [...resultSet][0]
+
+	const target = await result.doFunction('bob', 'foo/bar')
+	t.is(target, 'wow')
+})
+
+test('doFunction with hide false runs doFunction', async t => {
+	t.plan(3)
+	const a = new ActionHive<string>()
+
+	const doFunction: ActionFunc<string> = (context, path) => {
+		t.is(context, 'bob')
+		t.is(path, 'foo/bar')
+		return 'wow'
+	}
+
+	a.add(/bar$/, doFunction, () => false)
+
+	const resultSet = a.list(/^foo\//)
+	const result = [...resultSet][0]
+
+	const target = await result.doFunction('bob', 'foo/bar')
+	t.is(target, 'wow')
+})
+
+test('doFunction with hide true skips doFunction and returns update menu path .', async t => {
+	t.plan(1)
+	const a = new ActionHive<string>()
+
+	const doFunction: ActionFunc<string> = (context, path) => {
+		t.is(context, 'bob')
+		t.is(path, 'foo/bar')
+		return 'wow'
+	}
+
+	a.add(/bar$/, doFunction, () => true)
+
+	const resultSet = a.list(/^foo\//)
+	const result = [...resultSet][0]
+
+	const target = await result.doFunction('bob', 'foo/bar')
+	t.is(target, '.')
+})
