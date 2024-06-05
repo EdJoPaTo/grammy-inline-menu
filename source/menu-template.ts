45dfd2bc4fdd6036e5708a96f71aae08529d60f2
Congiary <66987756+Congiary@users.noreply.github.com>
2024-06-06 00:19:41 +0700

    docs(jsdoc): correct submenu function args order (#224)

--- source/menu-template.ts
+++ source/menu-template.ts
@@ -268,7 +268,7 @@ export class MenuTemplate<Context> {
 	 * })
 	 * submenuTemplate.manualRow(createBackMainMenuButtons())
 	 *
-	 * menuTemplate.submenu('enter submenu', 'unique', submenuTemplate)
+	 * menuTemplate.submenu('unique', submenuTemplate, { text: 'enter submenu' })
 	 */
 	submenu(
 		uniqueIdentifier: string,
